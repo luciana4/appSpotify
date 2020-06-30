f00f135af3012b05d6bd246f8385658350ccdf16
luciana4 <57041595+luciana4@users.noreply.github.com>
2020-06-29 23:52:34 -0500

    update query to bdd Classicmodel

--- routes/routes.js
+++ routes/routes.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
     var todoList = require('../controllers/controller');
 
     app.route('/')
-        .get(todoList.formulario);
+        .get(todoList.index);
 
     app.route('/vistaUsuarios')
      .get(todoList.vista);
